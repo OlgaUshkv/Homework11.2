a0060e85a286a44097f6332ed7992be7ed6b737d
OlgaUshkv <165587448+OlgaUshkv@users.noreply.github.com>
2024-04-02 22:28:11 +0300

    hotfix str_func.py

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
-def change_to_upper(my_string):
+def change_to_upper(my_string) -> str:
     """выводит слова заглавными буквами"""
     return my_string.upper()
 
 
-def change_to_title(my_string):
+def change_to_title(my_string) -> str:
     """каждое слово выводит с заглавной буквы"""
     return my_string.title()

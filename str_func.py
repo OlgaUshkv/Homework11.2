680e44368683af5d02898081c6088bc0375f19d3
Olga Yushkova <olga_ushkova93@list.ru>
2024-04-02 22:09:49 +0300

    add new func and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def change_to_upper(my_string):
+    """выводит слова заглавными буквами"""
     return my_string.upper()
+
+
+def change_to_title(my_string):
+    """каждое слово выводит с заглавной буквы"""
+    return my_string.title()
